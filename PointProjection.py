7a2e7cd24cb7274fd1ff7cc520ebf1a84961d8ad
Ole28 <29503359+Ole28@users.noreply.github.com>
2026-07-17 13:09:31 +0200

    kaputt work in progress

--- PointProjection.py
+++ PointProjection.py
@@ -5,8 +5,11 @@ import numpy as np
 type Point = np.ndarray
 type Vector = np.ndarray
 
-def projectPointOnEdge(p: Point, edgeStart: Point, edgeEnd: Point) -> Tuple[Point, float] :
-    if (len(edgeStart) != len(edgeStart) and len(edgeStart) != len(p)):
+
+def projectPointOnEdge(
+    p: Point, edgeStart: Point, edgeEnd: Point
+) -> Tuple[Point, float]:
+    if len(edgeStart) != len(edgeStart) and len(edgeStart) != len(p):
         raise Exception("No valid parameters for projection")
 
     lineVec: Vector = np.subtract(edgeEnd, edgeStart)
@@ -14,9 +17,9 @@ def projectPointOnEdge(p: Point, edgeStart: Point, edgeEnd: Point) -> Tuple[Poin
 
     t: float = np.dot(pointVec, lineVec) / np.dot(lineVec, lineVec)
 
-    if (t < 0):
+    if t < 0:
         return (edgeStart, t)
-    elif (t > 1):
+    elif t > 1:
         return (edgeEnd, t)
     else:
         p_proj = edgeStart + t * lineVec
--- RRT_with_Edge_sampling.py
+++ RRT_with_Edge_sampling.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List
+from typing import Any, Dict, List, NamedTuple, Optional, Tuple, TypedDict
 
 import networkx as nx
 import numpy as np
@@ -7,24 +7,90 @@ from scipy.spatial import KDTree
 from lib.IPEnvironment import CollisionChecker
 from lib.IPPerfMonitor import IPPerfMonitor
 from lib.IPPRMBase import PRMBase
+from PointProjection import projectPointOnEdge
 
 
-class RRTEdgeSampling(PRMBase):
+class configDict(TypedDict):
+    numberOfGeneratedNodes: int
+    testGoalAfterNumberOfNodes: int
+    stepSize: float
+    balanceTree: bool
+    sampleGoalProbability: float
+    collisionDetectionSteps: int
+    maxIterations: int
 
-    def __init__(self, _collChecker: CollisionChecker):
+
+class ProjectedPoint(NamedTuple):
+    point: np.ndarray
+    t: float
+    edge_start_id: int
+    edge_end_id: int
+
+
+type Point = np.ndarray
+
+
+class RRTEdge(PRMBase):
+
+    def __init__(self, collChecker: CollisionChecker):
         """
         _collChecker: the collision checker interface
         """
-        super(RRTEdgeSampling, self).__init__(_collChecker)
+        super(RRTEdge, self).__init__(collChecker)
         self.lastGeneratedNodeNumber: int = 0
+
+    def divide_edge(self, edgePoint: Point, start_Id: int, end_Id: int):
+        # Remove old edge
+        print("removing edge", start_Id, end_Id)
+        self.graph.remove_edge(start_Id, end_Id)
+
+        # Add new edges and point
+        print(
+            "adding node",
+            self.lastGeneratedNodeNumber,
+            "at position",
+            edgePoint,
+        )
+        self.graph.add_node(self.lastGeneratedNodeNumber, pos=edgePoint)
+
+        self.graph.add_edge(start_Id, self.lastGeneratedNodeNumber)
+        self.graph.add_edge(self.lastGeneratedNodeNumber, end_Id)
+
+        self.lastGeneratedNodeNumber += 1
+
+        # Check if graph is still acyclic and connected
+        if not nx.is_tree(self.graph):
+            raise Exception("The graph is either not connected or not acyclic")
+
+        return self.lastGeneratedNodeNumber - 1
+
+    def sampleNextPosition(
+        self,
+        sampleGoalProbability: float,
+    ) -> np.ndarray:
+        if sampleGoalProbability > np.random.rand():
+            goalNode = self.graph.nodes[1]
+            return np.asarray(goalNode["pos"])  # type: ignore
+        return self._getRandomFreePosition()
+
+    def stepTowardCandidate(
+        self,
+        candidatePos: np.ndarray,
+        nearestNeighborPos: np.ndarray,
+        stepSize: float,
+    ) -> np.ndarray:
+        diff = candidatePos - nearestNeighborPos
+        dist = np.linalg.norm(diff)
+        thisStep = min(dist, stepSize)
+        return nearestNeighborPos + (diff / dist) * thisStep
 
     @IPPerfMonitor
     def planPath(  # pyright: ignore[reportIncompatibleVariableOverride]
         self,
         startList: List[np.ndarray],
         goalList: List[np.ndarray],
-        config: Dict[str, Any],
-    ) -> List[np.ndarray]:
+        config: configDict,
+    ) -> Tuple[List[np.ndarray], Optional[str]]:
         """
 
         Args:
@@ -35,6 +101,8 @@ class RRTEdgeSampling(PRMBase):
         Example:
             config["numberOfGeneratedNodes"] = 500
             config["testGoalAfterNumberOfNodes"]  = 10
+            config["balanceTree"] = True
+            config["stepSize"] = 1.0
         """
         # 0. reset
         self.graph.clear()
@@ -47,47 +115,153 @@ class RRTEdgeSampling(PRMBase):
 
         # 2. add start and goal to graph
         self.graph.add_node(
-            self.lastGeneratedNodeNumber, pos=checkedStartList[0]
+            0,
+            pos=np.asarray(checkedStartList[0]),
         )
         self.lastGeneratedNodeNumber += 1
 
-        while self.lastGeneratedNodeNumber < config["numberOfGeneratedNodes"]:
+        stepSize = config.get("stepSize", np.inf)
 
-            posList = self.getNodePositions()
-            kdTree = KDTree(posList)
+        numIterations: int = 0
+        maxIterations: int = config.get("maxIterations", 10000)
+        sampleGoalProbability: float = config.get("sampleGoalProbability", 0.0)
+
+        collisionDetectionSteps = config.get("collisionDetectionSteps", 40)
+        while self.lastGeneratedNodeNumber < config["numberOfGeneratedNodes"]:
+            if numIterations >= maxIterations:
+                return [], "max_iterations_reached"
+            numIterations += 1
 
             if (
                 self.lastGeneratedNodeNumber
                 % config["testGoalAfterNumberOfNodes"]
             ) == 0:
+                proj_points: List[ProjectedPoint] = []
 
-                result = kdTree.query(checkedGoalList[0], k=1)
-
+                edges = self.graph.edges()
+                for u, v in edges:
+                    u_pos = self.graph.nodes[u]["pos"]
+                    v_pos = self.graph.nodes[v]["pos"]
+                    proj = projectPointOnEdge(checkedGoalList[0], u_pos, v_pos)
+                    p = ProjectedPoint(
+                        point=proj[0],
+                        t=proj[1],
+                        edge_start_id=u,
+                        edge_end_id=v,
+                    )
+                    proj_points.append(p)
+                pos_list = [x.point for x in proj_points]
+                print("test goal pos_list", pos_list)
+                kd_tree = KDTree(pos_list)
+                _, nearest_index_in_projection_points = kd_tree.query(
+                    checkedGoalList[0], k=1
+                )
+                nearest_proj_point = proj_points[
+                    nearest_index_in_projection_points
+                ]
                 if not self.collisionChecker.lineInCollision(
-                    self.graph.nodes[result[1]]["pos"], checkedGoalList[0]
+                    nearest_proj_point.point,
+                    checkedGoalList[0],
+                    steps=collisionDetectionSteps,
                 ):
-                    self.graph.add_node("goal", pos=checkedGoalList[0])
-                    self.graph.add_edge(result[1], "goal")
+                    self.graph.add_node(
+                        self.lastGeneratedNodeNumber,
+                        pos=checkedGoalList[0],
+                    )
+                    self.lastGeneratedNodeNumber += 1
+
+                    if nearest_proj_point.t > 0 and nearest_proj_point.t < 1:
+                        new_node_id = self.divide_edge(
+                            nearest_proj_point.point,
+                            nearest_proj_point.edge_start_id,
+                            nearest_proj_point.edge_end_id,
+                        )
+                        self.graph.add_edge(new_node_id, "goal")
+                    elif nearest_proj_point.t <= 0:
+                        self.graph.add_edge(
+                            nearest_proj_point.edge_start_id,
+                            "goal",
+                        )
+                    else:
+                        self.graph.add_edge(
+                            nearest_proj_point.edge_end_id,
+                            "goal",
+                        )
                     mapping = {0: "start"}
                     self.graph = nx.relabel_nodes(self.graph, mapping)
+                    return self.getShortestPathFromStartToGoal(), None
 
-                    return self.getShortestPathFromStartToGoal()
+            random_free_pos = self.sampleNextPosition(sampleGoalProbability)
 
-            pos = self._getRandomFreePosition()
+            edges = self.graph.edges()
 
-            # for every node in graph find nearest neigbhours
-
-            result = kdTree.query(pos, k=1)
-            if None:
-                raise Exception(
-                    "Something went wrong regarding nearest neighbours"
+            if not edges:
+                start = self.graph.nodes[0]["pos"]
+                candidate_step = self.stepTowardCandidate(
+                    random_free_pos, start, stepSize
                 )
-            # print result
+                if not self.collisionChecker.lineInCollision(
+                    start, candidate_step, steps=collisionDetectionSteps
+                ):
+                    self.graph.add_node(
+                        self.lastGeneratedNodeNumber,
+                        pos=candidate_step,
+                    )
+                    self.graph.add_edge(0, self.lastGeneratedNodeNumber)
+                    self.lastGeneratedNodeNumber += 1
+                continue
+
+            proj_points: List[ProjectedPoint] = []
+            for u, v in edges:
+                u_pos = self.graph.nodes[u]["pos"]
+                v_pos = self.graph.nodes[v]["pos"]
+                proj = projectPointOnEdge(random_free_pos, u_pos, v_pos)
+                p = ProjectedPoint(
+                    point=proj[0], t=proj[1], edge_start_id=u, edge_end_id=v
+                )
+                proj_points.append(p)
+            pos_list = [x.point for x in proj_points]
+            print("pos_list", pos_list)
+            kd_tree = KDTree(pos_list)
+            _, nearest_index_in_projection_points = kd_tree.query(
+                random_free_pos, k=1
+            )
+            nearest_proj_point = proj_points[
+                nearest_index_in_projection_points
+            ]
+
+            candidate_step = self.stepTowardCandidate(
+                random_free_pos, nearest_proj_point.point, stepSize
+            )
+
             if not self.collisionChecker.lineInCollision(
-                self.graph.nodes[result[1]]["pos"], pos
+                nearest_proj_point.point,
+                candidate_step,
+                steps=collisionDetectionSteps,
             ):
-                self.graph.add_node(self.lastGeneratedNodeNumber, pos=pos)
-                self.graph.add_edge(result[1], self.lastGeneratedNodeNumber)
+                self.graph.add_node(
+                    self.lastGeneratedNodeNumber,
+                    pos=candidate_step,
+                )
+                candidate_node_id = self.lastGeneratedNodeNumber
                 self.lastGeneratedNodeNumber += 1
 
-        return []
+                if nearest_proj_point.t > 0 and nearest_proj_point.t < 1:
+                    new_node_id = self.divide_edge(
+                        nearest_proj_point.point,
+                        nearest_proj_point.edge_start_id,
+                        nearest_proj_point.edge_end_id,
+                    )
+                    self.graph.add_edge(new_node_id, candidate_node_id)
+                elif nearest_proj_point.t <= 0:
+                    self.graph.add_edge(
+                        nearest_proj_point.edge_start_id,
+                        candidate_node_id,
+                    )
+                else:
+                    self.graph.add_edge(
+                        nearest_proj_point.edge_end_id,
+                        candidate_node_id,
+                    )
+
+        return [], "max_nodes_reached"
