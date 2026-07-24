8ffa49db2aa10320c47ada79661ce91590763532
Loretta Jacobs <jacobs>
2026-07-24 11:19:56 +0200

    Add Unittest for divide_edge

--- test_divide_edge.py
+++ test_divide_edge.py
@@ -32,5 +32,44 @@ class TestDivideEdge(unittest.TestCase):
 
         self.assertTrue(nx.is_tree(self.graph))
 
+    def test_divided_Edge_moreNodes(self):
+        self.graph.add_node(self.lastGeneratedNodeNumber, pos=[0,0])
+        firstPointId = self.lastGeneratedNodeNumber
+        self.lastGeneratedNodeNumber += 1
+
+        self.graph.add_node(self.lastGeneratedNodeNumber, pos=[5,0])
+        secondPointId = self.lastGeneratedNodeNumber
+        self.lastGeneratedNodeNumber += 1
+
+        self.graph.add_edge(firstPointId, secondPointId)
+
+        self.graph.add_node(self.lastGeneratedNodeNumber, pos=[0,3])
+        thirdPointId = self.lastGeneratedNodeNumber
+        self.lastGeneratedNodeNumber += 1
+
+        self.graph.add_edge(firstPointId, thirdPointId)
+        
+       
+        start = [5,3]
+        startId = self.lastGeneratedNodeNumber
+        self.lastGeneratedNodeNumber += 1
+        end = [7, 6]
+        endId = self.lastGeneratedNodeNumber
+        self.lastGeneratedNodeNumber += 1
+
+        self.graph.add_node(startId, pos=start)
+        self.graph.add_node(endId, pos=end)
+        self.graph.add_edge(secondPointId, startId)
+        self.graph.add_edge(startId, endId)
+
+        point = [6, 4]   
+        pointId = divide_edge(self, point, startId, endId)
+
+        self.assertEqual(len(self.graph.nodes), 6)
+        self.assertTrue(self.graph.has_edge(startId, pointId))
+        self.assertTrue(self.graph.has_edge(pointId, endId))
+
+        self.assertTrue(nx.is_tree(self.graph))
+
     def tearDown(self):
         self.graph.clear()
